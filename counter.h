d7594b17e09ff9152839a00b1cd3b068bdd288a4
Kundyukov <116679446+Kundyukov@users.noreply.github.com>
2022-10-25 23:09:27 +0300

    Add files via upload

---
+++ counter.h
@@ -0,0 +1,15 @@
+//#pragma once
+//class Counter
+//{
+//	int* massive;
+//	int length;
+//	int countzero;
+//public:
+//	void increase();
+//	void decrease();
+//	void initMas();
+//	void currentstate();
+//	Counter(int len);
+//	~Counter();
+//};
+//
